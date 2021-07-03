36843b6163f67b7a5577c86aa37d6f26db591ad0
Anand Dhakane <ananddhakane01@gmail.com>
2021-07-03 22:48:49 +0530

    react first tha day15

---
+++ React THA's/day15/meme-app/src/Components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Card from './Card'
+import './container.css'
+
+
+function AppMain() {
+    return (
+        <div className="container">
+            <Card />
+        </div>
+    )
+}
+
+export default AppMain
---
+++ React THA's/day15/meme-app/src/Components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Card() {
+    return (
+        <div className="card">
+            <img src="https://i.pinimg.com/originals/af/8d/63/af8d63a477078732b79ff9d9fc60873f.jpg" alt=""/>
+            <h1>meme app</h1>
+            <p>This is a meme Card</p>
+        </div>
+    )
+}
+
+export default Card
