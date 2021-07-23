7d67b359a9b1401c1d7e6fd497e357ecf38c5453
Anand Dhakane <ananddhakane01@gmail.com>
2021-07-23 15:35:27 +0530

    tha20

---
+++ React THA's/all-react-tha/src/components/THA20/Tha20.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import CalorieCard from './CalorieCard'
+import data from './data'
+import './styles.css'
+
+
+export default function THA20() {
+    // state
+    const [items, setItems] = useState(data);
+
+    const deleteCalCard = (food) => {
+        let updatedItems = items.filter(item => item.foodName !== food)
+        setItems(updatedItems);
+    }
+    const cardComponents = items.map(item => {
+        return <CalorieCard key={item.id} foodName={item.foodName} calorieNo={item.calorieNo} deleteCalCard = {deleteCalCard}/>
+    })
+
+    return (
+        <div className="main">
+            <h2>Calorie Tracker</h2>
+            <div className="container">
+                {cardComponents}
+            </div>
+        </div>
+    )
+}
